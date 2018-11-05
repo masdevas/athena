b31a2d9fa4ffcf75d8506b2c09061682970133bf
Alex <alexbatashev@gmail.com>
2018-11-05 17:06:58 +0300

    Add basic tests for TensorShape

---
+++ tests/unit/core/TensorShape.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Athena. All rights reserved.
+ * https://athenaframework.ml
+ *
+ * Licensed under MIT license.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include <gtest/gtest.h>
+#include <athena/core/TensorShape.h>
+
+TEST(TensorShapeTest, Creation) {
+    TensorShape({1, 2, 3});
+}
+
+TEST(TensorShapeTest, Dimension) {
+    TensorShape shape({1, 2, 3, 4});
+    ASSERT_EQ(shape.dimesions(), 4);
+}
+
+TEST(TensorShapeTest, TotalSize) {
+    TensorShape shape({1, 2, 3, 4});
+    ASSERT_EQ(shape.getTotalSize(), 1*2*3*4);
+}
