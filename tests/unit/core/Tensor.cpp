af92c51c2ac14b2cc139d484b7cfb2c340c64c21
masdevas <mork.andrey.s.dev@gmail.com>
2019-02-10 21:31:00 +0300

    Add subtensor's tests for Tensor

--- tests/unit/core/Tensor.cpp
+++ tests/unit/core/Tensor.cpp
@@ -25,11 +25,18 @@ TEST(TensorTest, Creation) {
     ASSERT_EQ(shape.getShape(), tensor.getShape().getShape());
 }
 
-TEST(TensorTest, OperatorIndexing) {
+TEST(TensorTest, GetSubtensor) {
     auto dataType = DataType::DOUBLE;
     size_t virtualAddress = 12;
     auto shape = TensorShape({2, 5, 8, 12});
     auto tensor = Tensor({dataType, virtualAddress, shape});
-    auto subtensor = tensor[50];
+    //tensor[50];   // it's working right (when FatalError())
+    size_t indexSubtensor = 1;
+    auto& subtensor = tensor[indexSubtensor];
+    ASSERT_EQ(subtensor.getDataType(), DataType::DOUBLE);
+    ASSERT_EQ(subtensor.getVirtualAddress(), virtualAddress +
+        indexSubtensor * shape.subshape().getTotalSize());
+    ASSERT_EQ(subtensor.getShape().getShape(), shape.subshape().getShape());
+    delete &subtensor;      // it is can make a problem with memory leak in other examples
 }
 }
