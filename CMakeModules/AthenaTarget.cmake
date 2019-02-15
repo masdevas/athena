cb15c9e2744ab021a21eac600ae59e3e937f8f9b
Alexander Batashev <alexbatashev@outlook.com>
2019-02-15 18:21:03 +0300

    Remove whole archive

--- CMakeModules/AthenaTarget.cmake
+++ CMakeModules/AthenaTarget.cmake
@@ -8,22 +8,6 @@ function(add_athena_library target_name modifier)
         athena_disable_rtti(${target_name})
         athena_disable_exceptions(${target_name})
     endif()
-
-    if("${modifier}" STREQUAL "SHARED")
-        if (WIN32)
-            set_target_properties(${target_name} PROPERTIES
-                    LINK_FLAGS "/WHOLEARCHIVE"
-                    )
-        elseif (APPLE)
-            set_target_properties(${target_name} PROPERTIES
-                    LINK_FLAGS "-Wl,-all_load"
-                    )
-        else ()
-            set_target_properties(${target_name} PROPERTIES
-                    LINK_FLAGS "-Wl,--whole-archive"
-                    )
-        endif ()
-    endif()
 endfunction(add_athena_library)
 
 function(add_athena_executable target_name)
