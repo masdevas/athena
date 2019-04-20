09847614d54a0922db9b09aab2358e6fcd65e344
Alexander Batashev <alexbatashev@outlook.com>
2019-04-20 21:59:40 +0300

    Exclude mode change from clang-format diff

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -7,7 +7,7 @@ else
     find include/ -name "*.h" | xargs clang-format-8 -i
     find src/ -iname "*.cpp" -o -iname "*.h" | xargs clang-format-8 -i
     find tests/ -iname "*.cpp" -o -iname "*.h" | xargs clang-format-8 -i
-    git diff -U0 --no-color > format-fixes.diff
+    git -c core.fileMode=false diff -U0 --no-color > format-fixes.diff
 fi
 
 git reset --hard HEAD
