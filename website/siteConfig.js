f715005371b9cfe8ec744edf2c93d5f902812abb
Alexander Batashev <alex@athenaframework.ml>
2019-05-28 10:32:01 +0000

    Update website configuration

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,16 +9,7 @@
 // site configuration options.
 
 // List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
+const users = [];
 
 const siteConfig = {
   title: 'Athena', // Title for your website.
@@ -100,6 +91,10 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
+
+  separateCSS: [
+    'api'
+  ]
 };
 
 module.exports = siteConfig;
