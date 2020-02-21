32a9185a2a2d232816c787497b60efade363e810
Alexander Batashev <alexbatashev@outlook.com>
2020-02-21 16:31:25 +0300

    feat(llvm_backend): New JIT compiler
    Features:
    1. Multi-threading
    2. Compile on demand with per module partitioning
    3. Remove MergeLayer as it has no value
    4. LLVM11 support

--- src/backend/llvm/jit/AthenaJIT.cpp
+++ src/backend/llvm/jit/AthenaJIT.cpp
@@ -1,49 +1,89 @@
-/*
- * Copyright (c) 2019 Athena. All rights reserved.
- * https://getathena.ml
- *
- * Licensed under MIT license.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+//===----------------------------------------------------------------------===//
+// Copyright (c) 2020 Athena. All rights reserved.
+// https://getathena.ml
+//
+// Licensed under MIT license.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+//===----------------------------------------------------------------------===//
 
 #include "AthenaJIT.h"
 
 #include <athena/core/FatalError.h>
 
-#include <cstdlib>
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Transforms/IPO/AlwaysInliner.h>
 #include <llvm/Transforms/IPO/PartialInlining.h>
 #include <llvm/Transforms/Scalar.h>
-#include <llvm/Transforms/Scalar/GVN.h>
+
+#include <cstdlib>
+
+using namespace llvm::orc;
 
 static size_t getNextFileId() {
   static size_t count = 0;
   return ++count;
 }
 
+static void explodeOnLazyCompileFailure() {
+  llvm::errs() << "Lazy compilation failed, Symbol Implmentation not found!\n";
+  exit(1);
+}
+
 namespace athena::backend::llvm {
 AthenaJIT::AthenaJIT(::llvm::orc::JITTargetMachineBuilder JTMB,
                      ::llvm::DataLayout&& DL)
-    : mObjectLayer(
-          mExecutionSession,
-          []() { return std::make_unique<::llvm::SectionMemoryManager>(); }),
-      mCompileLayer(mExecutionSession, mObjectLayer,
-                    ::llvm::orc::ConcurrentIRCompiler(std::move(JTMB))),
-      mOptimizeLayer(mExecutionSession, mCompileLayer, optimizeModule),
-      mMergeLayer(mExecutionSession, mOptimizeLayer), mDataLayout(DL),
-      mMangle(mExecutionSession, mDataLayout),
+    : mDataLayout(DL), mMangle(mExecutionSession, mDataLayout),
       mMainJD(mExecutionSession.createJITDylib("<main>")),
       mContext(std::make_unique<::llvm::LLVMContext>()) {
   ::llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
-  mMainJD.addGenerator(
-      cantFail(::llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
-          DL.getGlobalPrefix())));
+
+  auto callThroughMgr = createLocalLazyCallThroughManager(
+      JTMB.getTargetTriple(), mExecutionSession,
+      ::llvm::pointerToJITTargetAddress(explodeOnLazyCompileFailure));
+
+  if (!callThroughMgr) {
+    new core::FatalError(core::ATH_FATAL_OTHER, "");
+  }
+
+  mCallThroughManager = std::move(*callThroughMgr);
+
+  mObjectLayer =
+      std::make_unique<RTDyldObjectLinkingLayer>(mExecutionSession, []() {
+        return std::make_unique<::llvm::SectionMemoryManager>();
+      });
+#if (LLVM_VERSION_MAJOR == 11)
+  auto irCompiler = std::make_unique<ConcurrentIRCompiler>(JTMB);
+#else
+  ConcurrentIRCompiler irCompiler(JTMB);
+#endif
+  mCompileLayer = std::make_unique<IRCompileLayer>(
+      mExecutionSession, *mObjectLayer, std::move(irCompiler));
+  mOptimizeLayer = std::make_unique<IRTransformLayer>(
+      mExecutionSession, *mCompileLayer, optimizeModule);
+
+  mCODLayer = std::make_unique<CompileOnDemandLayer>(
+      mExecutionSession, *mOptimizeLayer, *mCallThroughManager,
+      createLocalIndirectStubsManagerBuilder(JTMB.getTargetTriple()));
+  mCODLayer->setImplMap(&mSymbolMap);
+
+  mExecutionSession.setDispatchMaterialization(
+      [this](JITDylib& jitDylib,
+             std::unique_ptr<MaterializationUnit> materializationUnit) {
+        auto matUnitPtr = std::shared_ptr<MaterializationUnit>(
+            std::move(materializationUnit));
+        auto Work = [matUnitPtr{std::move(matUnitPtr)}, &jitDylib]() {
+          matUnitPtr->doMaterialize(jitDylib);
+        };
+        mCompileThreads.async(std::move(Work));
+      });
+  mCODLayer->setPartitionFunction(CompileOnDemandLayer::compileWholeModule);
+
+  setUpJITDylib(&mMainJD);
 }
 std::unique_ptr<AthenaJIT> AthenaJIT::create() {
   LLVMInitializeNativeTarget();
@@ -67,17 +107,17 @@ std::unique_ptr<AthenaJIT> AthenaJIT::create() {
   return std::make_unique<AthenaJIT>(std::move(*JTMB), std::move(*DL));
 }
 ::llvm::Error AthenaJIT::addModule(std::unique_ptr<::llvm::Module>& M) {
-  return mMergeLayer.add(mMainJD,
-                         ::llvm::orc::ThreadSafeModule(std::move(M), mContext),
-                         mExecutionSession.allocateVModule());
+  return mCODLayer->add(mMainJD,
+                        ::llvm::orc::ThreadSafeModule(std::move(M), mContext));
 }
 ::llvm::Expected<::llvm::JITEvaluatedSymbol>
-AthenaJIT::lookup(::llvm::StringRef Name) {
-  return mExecutionSession.lookup({&mMainJD}, mMangle(Name.str()));
+AthenaJIT::lookup(::llvm::StringRef name) {
+  return mExecutionSession.lookup({&mMainJD}, mMangle(name.str()));
 }
 ::llvm::Expected<::llvm::orc::ThreadSafeModule>
 AthenaJIT::optimizeModule(::llvm::orc::ThreadSafeModule TSM,
-                          const ::llvm::orc::MaterializationResponsibility& R) {
+                          const ::llvm::orc::MaterializationResponsibility&
+                              materializationResponsibility) {
 #ifdef DEBUG
   size_t fileId = getNextFileId();
   std::error_code errorCode;
@@ -100,7 +140,6 @@ AthenaJIT::optimizeModule(::llvm::orc::ThreadSafeModule TSM,
 
   ::llvm::FunctionPassManager mFunctionSimplificationPassManager;
   ::llvm::ModulePassManager mModuleOptimizationPassManager;
-  ::llvm::ModulePassManager mThinLTOPreLinkPassManager;
   ::llvm::ModulePassManager mDefaultIPOPassManager;
 
   ::llvm::LoopAnalysisManager loopAnalysisManager;
@@ -116,20 +155,17 @@ AthenaJIT::optimizeModule(::llvm::orc::ThreadSafeModule TSM,
   passBuilder.crossRegisterProxies(loopAnalysisManager, functionAnalysisManager,
                                    cGSCCAnalysisManager, moduleAnalysisManager);
   mModuleOptimizationPassManager = passBuilder.buildModuleOptimizationPipeline(
-      ::llvm::PassBuilder::O2, false);
+      ::llvm::PassBuilder::OptimizationLevel::O2, false);
   mFunctionSimplificationPassManager =
       passBuilder.buildFunctionSimplificationPipeline(
-          ::llvm::PassBuilder::O2, ::llvm::PassBuilder::ThinLTOPhase::PostLink,
-          false);
-  mThinLTOPreLinkPassManager =
-      passBuilder.buildThinLTOPreLinkDefaultPipeline(::llvm::PassBuilder::O3);
+          ::llvm::PassBuilder::OptimizationLevel::O2,
+          ::llvm::PassBuilder::ThinLTOPhase::PostLink, false);
 
   mDefaultIPOPassManager.addPass(::llvm::AlwaysInlinerPass());
   mDefaultIPOPassManager.addPass(::llvm::PartialInlinerPass());
 
   TSM.withModuleDo([&](::llvm::Module& module) {
     mModuleOptimizationPassManager.run(module, moduleAnalysisManager);
-    mThinLTOPreLinkPassManager.run(module, moduleAnalysisManager);
     mDefaultIPOPassManager.run(module, moduleAnalysisManager);
 
     for (auto& func : module) {
@@ -155,6 +191,23 @@ AthenaJIT::optimizeModule(::llvm::orc::ThreadSafeModule TSM,
 #endif
 
   return TSM;
+}
+AthenaJIT::~AthenaJIT() { mCompileThreads.wait(); }
+void AthenaJIT::setUpJITDylib(JITDylib* jitDylib) {
+  LocalCXXRuntimeOverrides cxxRuntimeOverrides;
+
+  auto err = cxxRuntimeOverrides.enable(*jitDylib, mMangle);
+  if (err) {
+    // todo print err message
+    new core::FatalError(core::ATH_FATAL_OTHER, "Unexpected JIT error");
+  }
+
+  char prefix = mDataLayout.getGlobalPrefix();
+  auto generator = DynamicLibrarySearchGenerator::GetForCurrentProcess(prefix);
+  if (!generator) {
+    new core::FatalError(core::ATH_FATAL_OTHER, "Failed to create generator");
+  }
+  jitDylib->addGenerator(std::move(*generator));
 }
 
 } // namespace athena::backend::llvm
--- src/backend/llvm/jit/AthenaJIT.h
+++ src/backend/llvm/jit/AthenaJIT.h
@@ -1,20 +1,18 @@
-/*
- * Copyright (c) 2019 Athena. All rights reserved.
- * https://getathena.ml
- *
- * Licensed under MIT license.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+//===----------------------------------------------------------------------===//
+// Copyright (c) 2020 Athena. All rights reserved.
+// https://getathena.ml
+//
+// Licensed under MIT license.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+//===----------------------------------------------------------------------===//
 
 #ifndef ATHENA_ATHENAJIT_H
 #define ATHENA_ATHENAJIT_H
-
-#include "MergeLayer.h"
 
 #include <athena/backend/llvm/llvm_export.h>
 
@@ -22,6 +20,7 @@
 #include <llvm/Analysis/CGSCCPassManager.h>
 #include <llvm/Analysis/LoopAnalysisManager.h>
 #include <llvm/ExecutionEngine/JITSymbol.h>
+#include <llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h>
 #include <llvm/ExecutionEngine/Orc/CompileUtils.h>
 #include <llvm/ExecutionEngine/Orc/Core.h>
 #include <llvm/ExecutionEngine/Orc/ExecutionUtils.h>
@@ -34,41 +33,78 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/PassManager.h>
+#include <llvm/Support/ThreadPool.h>
+
+#include <atomic>
 #include <memory>
+#include <thread>
 
 namespace athena::backend::llvm {
-/**
- * Execute LLVM IR
- */
+
+/// AthenaJIT executes generated graph through LLVM's ORC.
+///
+/// The following layers are used during compilation phase:
+/// 1. Compile On Demand layer with whole module partitioning
+/// 2. IR transformation layer with O2 optimization pipeline
+/// 3. IR compilation layer
+/// 4. Object layer
+///
+/// Although graph consists of a single translation unit, the compilation
+/// is done in multiple threads to assist the case when we add runtime modules.
 class ATH_BACKEND_LLVM_EXPORT AthenaJIT {
 private:
   ::llvm::orc::ExecutionSession mExecutionSession;
-  ::llvm::orc::RTDyldObjectLinkingLayer mObjectLayer;
-  ::llvm::orc::IRCompileLayer mCompileLayer;
-  ::llvm::orc::IRTransformLayer mOptimizeLayer;
-  MergeLayer mMergeLayer;
+
+  std::unique_ptr<::llvm::orc::LazyCallThroughManager> mCallThroughManager;
+
+  std::unique_ptr<::llvm::orc::RTDyldObjectLinkingLayer> mObjectLayer;
+  std::unique_ptr<::llvm::orc::IRCompileLayer> mCompileLayer;
+  std::unique_ptr<::llvm::orc::IRTransformLayer> mOptimizeLayer;
+  std::unique_ptr<::llvm::orc::CompileOnDemandLayer> mCODLayer;
+
   ::llvm::DataLayout mDataLayout;
 
   ::llvm::orc::MangleAndInterner mMangle;
   ::llvm::orc::JITDylib& mMainJD;
   ::llvm::orc::ThreadSafeContext mContext;
 
+  ::llvm::ThreadPool mCompileThreads{::llvm::hardware_concurrency()};
+
+  ::llvm::orc::ImplSymbolMap mSymbolMap;
+
   static ::llvm::Expected<::llvm::orc::ThreadSafeModule>
   optimizeModule(::llvm::orc::ThreadSafeModule TSM,
-                 const ::llvm::orc::MaterializationResponsibility& R);
+                 const ::llvm::orc::MaterializationResponsibility&
+                     materializationResponsibility);
+
+  void setUpJITDylib(::llvm::orc::JITDylib* jitDylib);
 
 public:
-  AthenaJIT(::llvm::orc::JITTargetMachineBuilder JTMB, ::llvm::DataLayout&& DL);
+  AthenaJIT(::llvm::orc::JITTargetMachineBuilder JTMB,
+            ::llvm::DataLayout&& materializationUnit);
   AthenaJIT(const AthenaJIT&) = delete;
   AthenaJIT& operator=(const AthenaJIT&) = delete;
-  ~AthenaJIT() = default;
+  ~AthenaJIT();
 
+  /// \return an instance of default AthenaJIT.
   static std::unique_ptr<AthenaJIT> create();
+
   const ::llvm::DataLayout& getDataLayout() const { return mDataLayout; }
   ::llvm::LLVMContext& getContext() { return *mContext.getContext(); }
 
-  ::llvm::Error addModule(std::unique_ptr<::llvm::Module>& M);
-  ::llvm::Expected<::llvm::JITEvaluatedSymbol> lookup(::llvm::StringRef Name);
+  /// Adds module to the compilation queue.
+  ///
+  /// The real compilation takes place only when module is requested.
+  ///
+  /// \param module is a unique pointer to the module being added.
+  /// \return non-empty error if addition failed.
+  ::llvm::Error addModule(std::unique_ptr<::llvm::Module>& module);
+
+  /// Finds a named symbol in compiled code.
+  ///
+  /// \param name is a name of JITTed symbol to find.
+  /// \return a pointer to compiled symbol.
+  ::llvm::Expected<::llvm::JITEvaluatedSymbol> lookup(::llvm::StringRef name);
 };
 } // namespace athena::backend::llvm
 
