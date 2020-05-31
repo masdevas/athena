71bcd602767599ab4af03a5fe95c48ed0c92ed93
Alexander Batashev <alexbatashev@gmail.com>
2020-05-31 10:55:52 +0300

    Fix build problems

--- utils/kernels/bin2c.py
+++ utils/kernels/bin2c.py
@@ -12,8 +12,6 @@ def bin2c(input):
   indent = 2
   byte_len = 6  # '0x00, '
 
-  header = 'extern std::array<uint8_t, %d> %s;' % (len(data), varname)
-
   out = 'std::array<uint8_t, %d> %s = {\n' % (len(data), varname)
   line = ''
   for byte in data:
@@ -26,30 +24,21 @@ def bin2c(input):
   # strip the last comma
   out = out.rstrip(', \n') + '\n'
   out += '};\n'
-  return header, out
+  return out
 
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--source', type=str)
   parser.add_argument('--header', type=str)
   parser.add_argument('input', action='append')
 
   args = parser.parse_args()
 
-  converted = '#include <array>\n\n'
-  converted += '#include "%s"\n\n' % (os.path.basename(args.header))
-
   header = '#include <array>\n#pragma once\n\n'
 
   for file in args.input:
-    hdr, out = bin2c(file)
-    header += hdr
-    converted += out
+    header += bin2c(file)
   
-  with open(args.source, 'w') as outfile:
-    outfile.write(converted)
-
   with open(args.header, 'w') as outfile:
     outfile.write(header)
 
